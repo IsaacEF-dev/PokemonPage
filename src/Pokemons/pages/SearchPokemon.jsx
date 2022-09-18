3baa43a14c25c322c109c72ce70613939357a340
IsaacEF-dev <isaacjoyita@gmail.com>
2022-09-18 15:11:46 -0500

    se empezo con la pagina de buscar

--- src/Pokemons/pages/SearchPokemon.jsx
+++ src/Pokemons/pages/SearchPokemon.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {FcSearch,IoClose} from 'react-icons/all'
 
 export const SearchPokemon = () => {
   return (
-    <div>SearchPokemon</div>
+    <div className="content__search">
+      <form action="" className="container__search">
+        
+        <input type="text" className="search" />
+        <FcSearch className='icon__search'/>
+      </form>
+      <div className="container__modal">
+        <img src='../../../assets/NoResults.png' alt="" className="img__modal" />
+        <div className="content__message">
+          <p className="message">No results found</p>
+          <div className="btn__close"><IoClose/></div>
+        </div>
+      </div>
+    </div>
   )
 }
